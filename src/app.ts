46ece09a9a9d3137a475f6626c8e88b91b53bafb
--delete <--delete>
2024-05-21 21:06:44 +0600

    project-setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from "cors"
+import express, { Application, Request, Response } from "express"
+const app: Application = express();
+
+//parsers
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("server runing")
+})
+
+
+export default app;
+
+
